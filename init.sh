989dc59925ff558d81341c2b63a61a7e7ec83dd7
Yuki Takeichi <yuki.takeichi@gmail.com>
2015-12-04 12:31:10 +0900

    initialize script

---
+++ init.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+files=.*
+
+for file in ${files}
+do
+  filepath="${PWD}/${file}"
+  homefile="${HOME}/${file}"
+  
+  test $file == ".git" && continue
+  test $file == ".gitignore" && continue
+  test $file == ".gitmodules" && continue
+  test $file == "." && continue
+  test $file == ".." && continue
+
+  if [ ! -e "${homefile}" ]; then
+    echo "${file} not exists, make symbolic link to ${homefile}."
+    ln -s "${filepath}" "${homefile}"
+  else
+    echo "${file} exists."
+  fi
+done
